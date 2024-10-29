4aa86ede4df6c4a6ca5b59965e7bcd612d8b2253
WeakyStar0 <marcloflucky57@gmail.com>
2024-10-29 16:09:11 +0000

    App.js start

---
+++ project-root/client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Helmet } from 'react-helmet';
+
+function App() {
+  return (
+    <div className="App">
+      <Helmet>
+        <title>ola</title>
+      </Helmet>
+      <header className="App-header">
+        <p>
+          🤓 toue
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
