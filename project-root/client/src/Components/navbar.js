e20a85b13201f3562e2db17e56e76da2c031e2e6
WeakyStar0 <marcloflucky57@gmail.com>
2024-11-14 12:46:39 +0000

    navbar fixes

--- project-root/client/src/Components/navbar.js
+++ project-root/client/src/Components/navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Styles/navbarStyle.css';
+import logo from '../img/LOGO Academico_Viseu_FC.svg';
 
 const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <img src={logo} alt="Logo" style={{ width: '70px' }} />
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -22,6 +23,7 @@ const Navbar = () => {
                     </ul>
                 </div>
             </nav>
+            <hr className="hr-white" />
         </div>
     );
 };
