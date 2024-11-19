119d00b3dc61366b978e6fa20e6dcd4fffe57a5f
WeakyStar0 <marcloflucky57@gmail.com>
2024-11-19 17:18:05 +0000

    tweaks

--- project-root/client/src/App.js
+++ project-root/client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../src/Components/navbar';
 import Footer from '../src/Components/footer';
--- project-root/client/src/Pages/Home.js
+++ project-root/client/src/Pages/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import '../Styles/Home.css';
 import cachecol from '../img/cachecol.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export const Home = () => {
     return (
         <div className="home-container">
-            <h1>Viriatos Scouting</h1>
+
+            <div className=''><h className='home-tit'>Viriatos Scouting</h></div>
 
 
-            <img className="cachecol-home1" src={cachecol} alt="cachecol"/>
+            <img className="cachecol-home1" src={cachecol} alt="cachecol" />
+            <img className="cachecol-home2" src={cachecol} alt="cachecol" />
 
         </div>
     );
