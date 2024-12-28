49f45a49765c168cae0721824178327c4c0e014a
WeakyStar0 <marcloflucky57@gmail.com>
2024-12-28 17:57:29 +0000

    a

---
+++ project-root/client/src/Pages/ReportPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+export const Home = () => {
+    return (
+        <div className="home-container">
+
+        </div>
+    );
+};
