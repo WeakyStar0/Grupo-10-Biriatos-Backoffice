4e0b624858ef7761996e8027e08680f02c003a19
x1cuu <francisco.fernandes0811@gmail.com>
2024-12-28 18:32:44 +0000

    mareque

--- project-root/client/src/Pages/ReportPage.js
+++ project-root/client/src/Pages/ReportPage.js
@@ -3,7 +3,7 @@ import '../Styles/Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-export const Home = () => {
+export const ReportPage = () => {
     return (
         <div className="home-container">
 
