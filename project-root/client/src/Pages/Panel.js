486f07dbf70dcdf0578f152c08d1d6525b590269
x1cuu <francisco.fernandes0811@gmail.com>
2025-01-05 18:35:03 +0000

    mudanças no painel

--- project-root/client/src/Pages/Panel.js
+++ project-root/client/src/Pages/Panel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../Styles/Panel.css'; // Importando o arquivo de estilos CSS
+import '../Styles/Panel.css'; 
 
 export const Panel = () => {
     return (
@@ -10,23 +10,65 @@ export const Panel = () => {
                     <h2>ADMIN PANEL</h2>
                 </div>
                 <div className="menu">
-                    <button className="menu-item">Consultar jogadores</button>
-                    <button className="menu-item">Criar jogador</button>
-                    <button className="menu-item">Consultar equipas</button>
-                    <button className="menu-item">Criar equipa</button>
-                    <button className="menu-item">Consultar jogos</button>
-                    <button className="menu-item">Criar jogo</button>
-                    <button className="menu-item">Consultar relatórios</button>
-                    <button className="menu-item">Gerir tarefas</button>
+                   
+                    <div className="menu-section">
+                        <div className="menu-icon">
+                            <img src="/icons/player-icon.png" alt="Jogadores" />
+                        </div>
+                        <button className="menu-item">Consultar jogadores</button>
+                        <button className="menu-item">Criar jogador</button>
+                    </div>
+
+                 
+                    <div className="menu-section">
+                        <div className="menu-icon">
+                            <img src="/icons/team-icon.png" alt="Equipas" />
+                        </div>
+                        <button className="menu-item">Consultar equipas</button>
+                        <button className="menu-item">Criar equipa</button>
+                    </div>
+
+                
+                    <div className="menu-section">
+                        <div className="menu-icon">
+                            <img src="/icons/match-icon.png" alt="Jogos" />
+                        </div>
+                        <button className="menu-item">Consultar jogos</button>
+                        <button className="menu-item">Criar jogo</button>
+                    </div>
+
+                   
+                    <div className="menu-section">
+                        <div className="menu-icon">
+                            <img src="/icons/report-icon.png" alt="Relatórios" />
+                        </div>
+                        <button className="menu-item">Consultar relatórios</button>
+                    </div>
+
+                    
+                    <div className="menu-section">
+                        <div className="menu-icon">
+                            <img src="/icons/task-icon.png" alt="Tarefas" />
+                        </div>
+                        <button className="menu-item">Gerir tarefas</button>
+                    </div>
                 </div>
             </div>
-            <div className="main-content">
-                <div className="greeting">
-                    <h3>Olá, Marcelo</h3>
+
+            
+            <div className="vertical-bars">
+                <div className="vertical-bar-3"></div>
+                <div className="vertical-bar-2"></div>
+                <div className="vertical-bar">
+                    <div className="greeting">
+                        <h3>Olá, Marcelo</h3>
+                    </div>
                 </div>
-                <div className="logout">
-                    <button>LOGOUT</button>
-                </div>
+            </div>
+
+            <div className="main-content"></div>
+            <div className="logout">
+                <button>LOGOUT</button>
             </div>
         </div>
     );
