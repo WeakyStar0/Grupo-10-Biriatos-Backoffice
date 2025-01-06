9d88b73fb5bc93f30a1848d9a060cd8248d51c97
x1cuu <francisco.fernandes0811@gmail.com>
2025-01-06 21:26:16 +0000

    toue

--- project-root/client/src/Pages/Panel.js
+++ project-root/client/src/Pages/Panel.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../Styles/Panel.css'; 
+import { useNavigate } from 'react-router-dom';
 
 export const Panel = () => {
+    const navigate = useNavigate(); // Hook do React Router para navegação
+
     return (
         <div className="panel-container">
             <div className="sidebar">
@@ -33,7 +36,12 @@ export const Panel = () => {
                         </div>
                         <div className="menu-buttons">
                             <button className="menu-item">Consultar equipas</button>
-                            <button className="menu-item">Criar equipa</button>
+                            <button 
+                            className="menu-item"
+                            onClick={() => navigate('/CriarEquipas')}
+                            >
+                            Criar equipa
+                            </button>
                         </div>
                     </div>
 
@@ -51,7 +59,12 @@ export const Panel = () => {
                             <img src="/task.svg" alt="Tarefas" />
                         </div>
                         <div className="menu-buttons">
-                            <button className="menu-item">Gerir tarefas</button>
+                            <button 
+                                className="menu-item"
+                                onClick={() => navigate('/tarefas')} // Navega para a rota "/tarefas"
+                            >
+                                Gerir tarefas
+                            </button>
                         </div>
                     </div>
                 </div>
