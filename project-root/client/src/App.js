aaaca3f252a24be46ecbecc75e9152b1d853e945
Tiriri420 <tomas2002pereira@gmail.com>
2025-02-05 21:15:16 +0000

    divisoes botoes

--- project-root/client/src/App.js
+++ project-root/client/src/App.js
@@ -8,29 +8,15 @@ import { Helmet } from 'react-helmet';
 
 const App = () => {
   const location = useLocation(); // Obtém a rota atual
-
   return (
     <div className="App">
       <Helmet>
         <title>Viriatos Scouting App</title>
       </Helmet>
-
       {/* Renderiza a Navbar somente se não estiver na página de login */}
       {location.pathname !== '/' && <Navbar />}
-
       <Outlet /> {/* Renderiza a página conforme a rota */}
-
-      {/* Renderiza o Footer somente nas rotas permitidas */}
-      {location.pathname !== '/' &&
-        location.pathname !== '/panel' &&
-        location.pathname !== '/tarefas' &&
-        location.pathname !== '/consultar-relatorio' &&
-        location.pathname !== '/criarutilizador' &&
-        location.pathname !== '/perfildejogador' &&
-        location.pathname !== '/lista-jogadores' &&
-        (
-          <Footer />
-        )}
+      {location.pathname === '/' && <Footer />}
     </div>
   );
 }
--- project-root/client/src/Pages/divisoes.js
+++ project-root/client/src/Pages/divisoes.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '../Styles/divisoes.css'; 
+import '../Styles/divisoes.css';
 
 export default function Divisoes() {
   const { clubName } = useParams(); // Captura o nome do clube da URL
   const navigate = useNavigate(); // Para voltar atrás
-
   return (
     <div className="container">
       {/* Painel Esquerdo */}
@@ -17,7 +16,6 @@ export default function Divisoes() {
           <h1>Viriatos Scouting</h1>
         </div>
         <h3 className="sub-title">JOGADORES - {clubName}</h3>
-
         <div className="menu">
           <button className="menu-button">Equipa profissional</button>
           <button className="menu-button">Sub-23</button>
@@ -26,10 +24,11 @@ export default function Divisoes() {
           <button className="menu-button">Formação Feminino <span>&gt;</span></button>
         </div>
       </div>
-
       {/* Painel Direito */}
-      <div className="right-panel">
-        <div className="button-container">
+      <div className="vertical-bars">
+        <div className="vertical-bar-3"></div>
+        <div className="vertical-bar-2"></div>
+        <div className="vertical-bar">
           <button className="primary-button">Novos jogadores</button>
           <button className="secondary-button" onClick={() => navigate(-1)}>Voltar</button>
         </div>
