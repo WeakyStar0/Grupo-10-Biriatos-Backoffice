f09057b69867ee79fb1b4b4ee0d8d44c5b455cca
x1cuu <francisco.fernandes0811@gmail.com>
2025-02-07 19:14:25 +0000

    criar jogador conexion

--- project-root/backend/models/Team.js
+++ project-root/backend/models/Team.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const teamSchema = new mongoose.Schema({
   teamId: { type: Number, required: true, unique: true },
   teamName: { type: String, required: true, unique: true },
+  Escalao: { type: String},
   teamType: { type: String, enum: ['Own', 'Shadow', 'Club'], required: true },
   tasks: { type: [Number], default: [] },
 });
